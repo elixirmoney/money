595ba9dc67cffe957b21f71a90a9e50a456ff279
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-05-25 09:03:08 +0200

    Replace tabs with spaces in tests

--- test/money/currency_test.exs
+++ test/money/currency_test.exs
@@ -22,38 +22,38 @@ defmodule Money.CurrencyTest do
     assert_raise ArgumentError, fn -> Currency.get!(:ABC) end
   end
 
-	test "name/1" do
-		assert Currency.name(:USD) == "US Dollar"
-		assert Currency.name(Currency.usd(100)) == "US Dollar"
-		assert Currency.name(:ABC) == nil
-	end
+  test "name/1" do
+    assert Currency.name(:USD) == "US Dollar"
+    assert Currency.name(Currency.usd(100)) == "US Dollar"
+    assert Currency.name(:ABC) == nil
+  end
 
-	test "name!/1" do
-		assert Currency.name!(:USD) == "US Dollar"
-		assert Currency.name!(Currency.usd(100)) == "US Dollar"
-		assert_raise ArgumentError, fn -> Currency.name!(:ABC) end
-	end
+  test "name!/1" do
+    assert Currency.name!(:USD) == "US Dollar"
+    assert Currency.name!(Currency.usd(100)) == "US Dollar"
+    assert_raise ArgumentError, fn -> Currency.name!(:ABC) end
+  end
 
-	test "symbol/1" do
-		assert Currency.symbol(:USD) == "$"
-		assert Currency.symbol(Currency.usd(100)) == "$"
-		assert Currency.symbol(:ABC) == nil
-	end
+  test "symbol/1" do
+    assert Currency.symbol(:USD) == "$"
+    assert Currency.symbol(Currency.usd(100)) == "$"
+    assert Currency.symbol(:ABC) == nil
+  end
 
-	test "symbol!/1" do
-		assert Currency.symbol!(:USD) == "$"
-		assert Currency.symbol!(Currency.usd(100)) == "$"
-		assert_raise ArgumentError, fn -> Currency.symbol!(:ABC) end
-	end
+  test "symbol!/1" do
+    assert Currency.symbol!(:USD) == "$"
+    assert Currency.symbol!(Currency.usd(100)) == "$"
+    assert_raise ArgumentError, fn -> Currency.symbol!(:ABC) end
+  end
 
-	test "to_atom/1" do
-		assert Currency.to_atom(:USD) == :USD
-		assert Currency.to_atom("USD") == :USD
-		assert Currency.to_atom("usd") == :USD
-		assert Currency.to_atom(Currency.usd(100)) == :USD
-		assert_raise ArgumentError, fn -> Currency.to_atom(:ABC) end
-		assert_raise ArgumentError, fn -> Currency.to_atom("ABC") end
-		assert_raise ArgumentError, fn -> Currency.to_atom("abc") end
-		assert_raise ArgumentError, fn -> Currency.to_atom("abc" <> "khgyujnk") end
-	end
+  test "to_atom/1" do
+    assert Currency.to_atom(:USD) == :USD
+    assert Currency.to_atom("USD") == :USD
+    assert Currency.to_atom("usd") == :USD
+    assert Currency.to_atom(Currency.usd(100)) == :USD
+    assert_raise ArgumentError, fn -> Currency.to_atom(:ABC) end
+    assert_raise ArgumentError, fn -> Currency.to_atom("ABC") end
+    assert_raise ArgumentError, fn -> Currency.to_atom("abc") end
+    assert_raise ArgumentError, fn -> Currency.to_atom("abc" <> "khgyujnk") end
+  end
 end
--- test/money_test.exs
+++ test/money_test.exs
@@ -1,5 +1,5 @@
 defmodule MoneyTest do
-	use ExUnit.Case
+  use ExUnit.Case
   doctest Money
 
   require Money.Currency
@@ -11,103 +11,103 @@ defmodule MoneyTest do
     end
   end
 
-	test "test factory USD" do
-		assert Money.new(123, :USD) == usd(123)
-	end
+  test "test factory USD" do
+    assert Money.new(123, :USD) == usd(123)
+  end
 
-	test "test factory EUR" do
-		assert Money.new(124, :EUR) == eur(124)
-	end
+  test "test factory EUR" do
+    assert Money.new(124, :EUR) == eur(124)
+  end
 
-	test "test factory with string" do
-		assert Money.new("+124", :EUR) == eur(124)
-	end
+  test "test factory with string" do
+    assert Money.new("+124", :EUR) == eur(124)
+  end
 
   test "use string currency key" do
     assert Money.new(1000, "USD") == usd(1000)
     assert Money.new(1000, "usd") == usd(1000)
   end
 
-	test "conversion error" do
-		assert_raise FunctionClauseError, fn ->
-		  Money.new(:atom, :USD)
-		end
-	end
+  test "conversion error" do
+    assert_raise FunctionClauseError, fn ->
+      Money.new(:atom, :USD)
+    end
+  end
 
-	test "test compare" do
-		assert Money.compare(Money.new(123, :USD), Money.new(123, :USD)) == 0
-		assert Money.compare(Money.new(121, :USD), Money.new(123, :USD)) == -1
-		assert Money.compare(Money.new(124, :USD), Money.new(123, :USD)) == 1
-	end
+  test "test compare" do
+    assert Money.compare(Money.new(123, :USD), Money.new(123, :USD)) == 0
+    assert Money.compare(Money.new(121, :USD), Money.new(123, :USD)) == -1
+    assert Money.compare(Money.new(124, :USD), Money.new(123, :USD)) == 1
+  end
 
-	test "comparing error" do
-		assert_raise ArgumentError, fn ->
-			Money.compare(Money.new(124, :EUR), Money.new(123, :USD))
-		end
-	end
+  test "comparing error" do
+    assert_raise ArgumentError, fn ->
+      Money.compare(Money.new(124, :EUR), Money.new(123, :USD))
+    end
+  end
 
-	test "test zero?" do
-		assert Money.zero?(Money.new(0, :USD))
-	end
+  test "test zero?" do
+    assert Money.zero?(Money.new(0, :USD))
+  end
 
-	test "test equals?" do
-		assert Money.equals?(Money.new(123, :USD), usd(123))
-	end
+  test "test equals?" do
+    assert Money.equals?(Money.new(123, :USD), usd(123))
+  end
 
-	test "test add" do
-		assert Money.add(Money.new(100, :USD), Money.new(200, :USD)) == Money.new(300, :USD)
-	end
+  test "test add" do
+    assert Money.add(Money.new(100, :USD), Money.new(200, :USD)) == Money.new(300, :USD)
+  end
 
-	test "add error" do
-		assert_raise ArgumentError, fn ->
-			Money.add(Money.new(124, :EUR), Money.new(123, :USD))
-		end
-	end
+  test "add error" do
+    assert_raise ArgumentError, fn ->
+      Money.add(Money.new(124, :EUR), Money.new(123, :USD))
+    end
+  end
 
-	test "test subtract" do
-		assert Money.subtract(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(100, :USD)
-	end
+  test "test subtract" do
+    assert Money.subtract(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(100, :USD)
+  end
 
-	test "subtract error" do
-		assert_raise ArgumentError, fn ->
-			Money.subtract(Money.new(124, :EUR), Money.new(123, :USD))
-		end
-	end
+  test "subtract error" do
+    assert_raise ArgumentError, fn ->
+      Money.subtract(Money.new(124, :EUR), Money.new(123, :USD))
+    end
+  end
 
-	test "test multiply" do
-		assert Money.multiply(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(20000, :USD)
-	end
+  test "test multiply" do
+    assert Money.multiply(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(20000, :USD)
+  end
 
-	test "test multiply with a multiplier" do
-		assert Money.multiply(Money.new(200, :USD), 3) == Money.new(600, :USD)
-	end
+  test "test multiply with a multiplier" do
+    assert Money.multiply(Money.new(200, :USD), 3) == Money.new(600, :USD)
+  end
 
-	test "multiply error" do
-		assert_raise ArgumentError, fn ->
-			Money.multiply(Money.new(124, :EUR), Money.new(123, :USD))
-		end
-	end
+  test "multiply error" do
+    assert_raise ArgumentError, fn ->
+      Money.multiply(Money.new(124, :EUR), Money.new(123, :USD))
+    end
+  end
 
-	test "test divide" do
-		assert Money.divide(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(2, :USD)
-	end
+  test "test divide" do
+    assert Money.divide(Money.new(200, :USD), Money.new(100, :USD)) == Money.new(2, :USD)
+  end
 
-	test "test divide2" do
-		assert Money.divide(Money.new(139, :USD), Money.new(113, :USD)) == Money.new(1, :USD)
-	end
+  test "test divide2" do
+    assert Money.divide(Money.new(139, :USD), Money.new(113, :USD)) == Money.new(1, :USD)
+  end
 
-	test "divide error" do
-		assert_raise ArgumentError, fn ->
-			Money.divide(Money.new(124, :EUR), Money.new(123, :USD))
-		end
-	end
+  test "divide error" do
+    assert_raise ArgumentError, fn ->
+      Money.divide(Money.new(124, :EUR), Money.new(123, :USD))
+    end
+  end
 
-	test "test to_string" do
-		assert Money.to_string(usd(500)) == "$5.00"
-		assert Money.to_string(eur(123)) == "€1.23"
-		assert Money.to_string(nad(203)) == "2.03"
-		assert Money.to_string(zar(1234567890)) == "R12,345,678.90"
-	end
+  test "test to_string" do
+    assert Money.to_string(usd(500)) == "$5.00"
+    assert Money.to_string(eur(123)) == "€1.23"
+    assert Money.to_string(nad(203)) == "2.03"
+    assert Money.to_string(zar(1234567890)) == "R12,345,678.90"
+  end
 
   test "to_string protocol" do
     m = usd(500)
