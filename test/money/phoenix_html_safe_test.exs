213dd71b12e0a930d444f5941b8d823561f714cb
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-06-30 12:19:57 +0200

    No need to use the application config in this test

--- test/money/phoenix_html_safe_test.exs
+++ test/money/phoenix_html_safe_test.exs
@@ -1,17 +1,9 @@
 if Code.ensure_compiled?(Phoenix.HTML.Safe) do
   defmodule PhoenixHTMLSafeTeset do
-    use ExUnit.Case, async: false
-
-    setup do
-      Application.put_env(:money, :default_currency, :GBP)
-
-      on_exit fn ->
-        Application.delete_env(:money, :default_currency)
-      end
-    end
+    use ExUnit.Case, async: true
 
     test "Phoenix.HTML.Safe for Money" do
-      money = Money.new(123456)
+      money = Money.new(123456, :EUR)
 
       assert Phoenix.HTML.Safe.to_iodata(money) == Phoenix.HTML.Safe.to_iodata(to_string(money))
     end
