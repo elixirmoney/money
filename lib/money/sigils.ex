ba964f5cf16a99248db38e80506b0665a6544bfb
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-07-18 12:01:38 +0200

    Add support for Money sigil ~M (#32)

---
+++ lib/money/sigils.ex
@@ -0,0 +1,20 @@
+defmodule Money.Sigils do
+  @doc ~S"""
+  Handles the sigil `~M` for Money
+
+  The lower case `~m` variant does not exist as interpolation and excape characters are not useful for Money sigils.
+
+  ## Usage
+
+      import Money.Sigils
+      ~M[1000] # With a configured default currency (e.g. GBP)
+      #> %Money{amount: 1000, currency: :GBP3}
+
+      ~M[1000]USD
+      #> %Money{amount: 1000, currency: :USD}
+  """
+  def sigil_M(amount, []),
+    do: Money.new(String.to_integer(amount))
+  def sigil_M(amount, [_,_,_]=currency),
+    do: Money.new(String.to_integer(amount), List.to_existing_atom(currency))
+end
---
+++ test/money/sigils_test.exs
@@ -0,0 +1,36 @@
+defmodule Money.SigilsTest do
+  use ExUnit.Case, async: true
+  doctest Money.Sigils
+
+  import Money.Sigils
+
+  setup do
+    Application.put_env(:money, :default_currency, :GBP)
+
+    on_exit fn ->
+      Application.delete_env(:money, :default_currency)
+    end
+  end
+
+  test "it can create a money object from a sigil" do
+    assert ~M[1000] == Money.new(1000, :GBP)
+  end
+
+  test "it can create a money object from a sigil with a currency" do
+    assert ~M[1000]USD == Money.new(1000, :USD)
+    assert ~M[1000]GBP == Money.new(1000, :GBP)
+    assert ~M[1000]EUR == Money.new(1000, :EUR)
+  end
+
+  test "it fails with anything other than a three character country code" do
+    assert_raise FunctionClauseError, fn ->
+      ~M[1000]U
+    end
+    assert_raise FunctionClauseError, fn ->
+      ~M[1000]US
+    end
+    assert_raise FunctionClauseError, fn ->
+      ~M[1000]EURO
+    end
+  end
+end
