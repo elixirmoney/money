de1fdc0a5cdb0151165c328eaf6b5892210c5bb6
Andrew Timberlake <andrew@andrewtimberlake.com>
2017-01-05 20:47:37 +0200

    Change sigil functions to macros so they are expanded at compile time

--- test/money/sigils_test.exs
+++ test/money/sigils_test.exs
@@ -4,13 +4,8 @@ defmodule Money.SigilsTest do
 
   import Money.Sigils
 
-  setup do
-    Application.put_env(:money, :default_currency, :GBP)
-
-    on_exit fn ->
-      Application.delete_env(:money, :default_currency)
-    end
-  end
+  # Set application setting before compilation of the followin tests
+  Application.put_env(:money, :default_currency, :GBP)
 
   test "it can create a money object from a sigil" do
     assert ~M[1000] == Money.new(1000, :GBP)
@@ -21,21 +16,24 @@ defmodule Money.SigilsTest do
     assert ~M[1_000_00] == Money.new(100000, :GBP)
   end
 
+  # Revert the settins after compilation of the above tests
+  Application.delete_env(:money, :default_currency)
+
   test "it can create a money object from a sigil with a currency" do
     assert ~M[1000]USD == Money.new(1000, :USD)
     assert ~M[1000]GBP == Money.new(1000, :GBP)
     assert ~M[1000]EUR == Money.new(1000, :EUR)
   end
 
-  test "it fails with anything other than a three character country code" do
-    assert_raise FunctionClauseError, fn ->
-      ~M[1000]U
+  # This is a convoluted test because I'm trying to test a failing compilation (I'd love a better way to do this)
+  try do
+    test "it fails to expand the sigil with an invalid currency code" do
+      m = ~M[1000]U
+      refute m, "This test should have failed"
     end
-    assert_raise FunctionClauseError, fn ->
-      ~M[1000]US
-    end
-    assert_raise FunctionClauseError, fn ->
-      ~M[1000]EURO
-    end
+  rescue
+    ArgumentError ->
+      test "it fails to expand the sigil with an invalid currency code",
+        do: assert Enum.random(1..2), "Failure to expand sigil for invalid currency"
   end
 end
