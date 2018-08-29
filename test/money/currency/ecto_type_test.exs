82493c55e6e82be253b3fe5a998fdc9cd5241ba7
Steven Blowers <sb@rekki.com>
2018-08-29 16:36:26 +0100

    Create new Ecto Type for Currency

---
+++ test/money/currency/ecto_type_test.exs
@@ -0,0 +1,43 @@
+if Code.ensure_compiled?(Ecto.Type) do
+  defmodule Money.Currency.Ecto.TypeTest do
+    use ExUnit.Case, async: false
+
+    alias Money.Currency.Ecto.Type
+
+    doctest Type
+
+    test "type/0" do
+      assert Type.type() == :string
+    end
+
+    test "cast/1 string that is currency code" do
+      assert Type.cast("USD") == {:ok, :USD}
+      assert Type.cast("sll") == {:ok, :SLL}
+    end
+
+    test "cast/1 string that is not currency code" do
+      assert Type.cast("xxx") == :error
+    end
+
+    test "cast/1 Money" do
+      assert Type.cast(Money.new(1000, :GBP)) == {:ok, :GBP}
+    end
+
+    test "cast/1 other" do
+      assert Type.cast(1) == :error
+      assert Type.cast(true) == :error
+    end
+
+    test "load/1 string" do
+      assert Type.load("TWD") == {:ok, :TWD}
+    end
+
+    test "dump/1 atom" do
+      assert Type.dump(:CHF) == {:ok, "CHF"}
+    end
+
+    test "dump/1 other" do
+      assert Type.dump({:a, :b}) == :error
+    end
+  end
+end
