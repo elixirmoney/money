8d34db0f3ca35e2573ba6ae7b986b7619041db9c
Steven Blowers <shdblowers@users.noreply.github.com>
2019-01-30 09:23:36 +0000

    Create new Ecto Type for Currency (#87)
    * Create new Ecto Type for Currency
    
    * Currency Ecto type casts atoms

---
+++ test/money/currency/ecto_type_test.exs
@@ -0,0 +1,51 @@
+if Code.ensure_compiled?(Ecto.Type) do
+  defmodule Money.Currency.Ecto.TypeTest do
+    use ExUnit.Case, async: false
+
+    alias Money.Currency.Ecto.Type
+
+    doctest Type
+
+    test "type/0" do
+      assert Type.type() == :string
+    end
+
+    test "cast/1 string that is currency code" do
+      assert Type.cast("USD") == {:ok, :USD}
+      assert Type.cast("sll") == {:ok, :SLL}
+    end
+
+    test "cast/1 string that is not currency code" do
+      assert Type.cast("xxx") == :error
+    end
+
+    test "cast/1 atom that is currency code" do
+      assert Type.cast(:BRL) == {:ok, :BRL}
+    end
+
+    test "cast/1 atom that is not currency code" do
+      assert Type.cast(:Z29) == :error
+    end
+
+    test "cast/1 Money" do
+      assert Type.cast(Money.new(1000, :GBP)) == {:ok, :GBP}
+    end
+
+    test "cast/1 other" do
+      assert Type.cast(1) == :error
+      assert Type.cast(true) == :error
+    end
+
+    test "load/1 string" do
+      assert Type.load("TWD") == {:ok, :TWD}
+    end
+
+    test "dump/1 atom" do
+      assert Type.dump(:CHF) == {:ok, "CHF"}
+    end
+
+    test "dump/1 other" do
+      assert Type.dump({:a, :b}) == :error
+    end
+  end
+end
